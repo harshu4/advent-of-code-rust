69bbbcda33f19828bb7c61060fa98867d122b08a
BoredAlchemist <harsh4444.hf@gmail.com>
2020-04-29 20:38:30 +0530

    day 6 started

--- rust/day6/src/main.rs
+++ rust/day6/src/main.rs
@@ -1,39 +1,77 @@
-use std::fs;
 use std::collections::HashMap;
-
-struct Obj<'a> {
+use std::fs;
+#[derive(Clone, Debug)]
+struct Obj {
     name: String,
-    follows: Option<&'a Obj<'a>>,
+    follows: Option<usize>,
 }
 
-impl<'a> Obj<'a> {
-    fn new(name: String) -> Obj<'a> {
+impl Obj {
+    fn new(name: String) -> Obj {
         Obj {
             name,
             follows: Option::None,
         }
     }
-    fn add(&mut self, a: &'a Obj) -> Result<(), &'static str> {
-        self.follows = Option::Some(a);
+    fn add(&mut self, a: usize) -> Result<(), &'static str> {
+        self.follows = Some(a);
         Ok(())
     }
 }
 
 fn main() {
     let content = fs::read_to_string("src/input.txt").unwrap();
-    let mut arr = HashMap::new();
-    
-    for x in content.lines(){
-        if !arr.contains_key(&x[4..7]){
-            arr.insert(x[4..7].to_string(),Obj::new(x[4..7].to_string()));
-        }
-        if !arr.contains_key(&x[0..3]){
-            arr.insert(x[0..3].to_string(),Obj::new(x[0..3].to_string()));
-        }
-        let mut abc = arr.get_mut(&x[4..7]).unwrap();
-        let obj = *arr.get(&x[0..3]).unwrap().clone();
-        
+    let mut arr: Vec<Obj> = Vec::new();
+    let mut a_found:i32 = -1;
+    let mut b_found:i32 = -1;
+    let mut index = 0;
 
+    for x in content.lines() {
+      
+        for y in &arr {
+            if y.name == &x[0..3] {
+               
+                b_found = index;
+                
+            }
+            
+            if y.name == &x[4..7] {
+                
+                a_found = index;
+            }
+            
+            index += 1;
+        }
+        index = 0;
+        if a_found == -1 {
+            arr.push(Obj::new(x[4..7].to_string()));
+            println!("{}",arr.len());
+            a_found = (arr.len() - 1) as i32;
+        }
 
+        if b_found == -1 {
+            arr.push(Obj::new(x[0..3].to_string()));
+            println!("{}",arr.len());
+            b_found = (arr.len() - 1) as i32 ;
+        }
+
+        arr[a_found as usize].add((b_found) as usize);
+
+        a_found = -1;
+        b_found = -1;
+       
     }
+    println!("{:?}",arr);
+    let mut count = 0;
+    let mut y = &arr[0];
+    for x in &arr {
+        y = x;
+        while y.follows.is_some() {
+            count += 1;
+            y = &arr[y.follows.unwrap()]
+
+        }
+        
+    }
+    println!("{}", count)
 }
